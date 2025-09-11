56d59cdb41f6f24dfb968d1e759151cf85c0c08d
Gladyson Rhuan <gladysonr04@gmail.com>
2025-09-11 18:03:51 -0300

    feat: Criação de endpoint para login.

--- backend/src/main/java/com/unifucamp/gamearena/utils/HomeController.java
+++ backend/src/main/java/com/unifucamp/gamearena/utils/HomeController.java
@@ -1,4 +1,4 @@
-package com.unifucamp.gamearena.controller;
+package com.unifucamp.gamearena.utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
