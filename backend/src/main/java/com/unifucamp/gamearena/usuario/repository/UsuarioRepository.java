c60ddc67e8325d345f128c1b46fc42e191b159cb
Gladyson Rhuan <gladysonr04@gmail.com>
2025-09-20 11:19:43 -0300

    UsuarioRepository.java

---
+++ backend/src/main/java/com/unifucamp/gamearena/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.unifucamp.gamearena.usuario.repository;
+
+import com.unifucamp.gamearena.usuario.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Optional<Usuario> findByEmail(String email);
+}
